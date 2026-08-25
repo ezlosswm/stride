1898732fae7e6d90142398307b0e6c588c38436b
ezlosswm <ezlosswm@gmail.com>
2026-08-24 22:27:30 -0600

    hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,115 +32,119 @@
 	}
 </script>
 
-<h2>Collections</h2>
-
-<main class="mt-10 min-h-dvh border-b">
-	<div class="relative -top-8 left-64 -z-40">
+<main class="mt-10 border-b">
+	<!-- Circle -->
+	<div class="pointer-events-none absolute inset-x-0 top-0 h-[60vh] overflow-x-clip">
 		<div
-			class="absolute top-1/2 left-1/2 size-36 -translate-1/2 rounded-full border border-accent bg-transparent shadow-[0px_0px_50px_50px] shadow-accent/16"
+			class="absolute top-24 right-0 size-48 translate-x-1/2 rounded-full border border-accent/50 shadow-[0px_0px_100px_100px] shadow-accent/12 sm:size-96 md:top-52"
 		>
-			<div
-				class="absolute top-1/2 left-1/2 size-28 -translate-1/2 rounded-full border bg-transparent"
-			>
-				<div
-					class="absolute top-1/2 left-1/2 size-20 -translate-1/2 rounded-full border bg-transparent"
-				></div>
-			</div>
+			<div class="absolute inset-1/2 size-32 -translate-1/2 rounded-full border sm:size-64"></div>
 		</div>
 	</div>
 
-	<div>
-		<div class="flex items-center gap-1.5">
-			<div
-				class="size-2 animate-pulse rounded-full bg-accent shadow-[0_0_30px_5px] shadow-accent/70"
-			></div>
-			<span class="text-xs font-medium tracking-wider text-muted-foreground uppercase"
-				>THE SMARTER WAY TO BUY SNEAKERS</span
-			>
-		</div>
-
-		<div class="space-y-3">
-			<h1 class="font-heading text-5xl font-bold tracking-tight">
-				<span> One pair. </span>
-				<br />
-				<span class="text-muted-foreground">Two markets.</span>
-				<br />
-				<span> One clear choice. </span>
-			</h1>
-			<p>
-				STRIDE compares sneaker prices across StockX and GOAT, so you can quickly see where to buy,
-				what you'll pay, and when the price is right.
-			</p>
-		</div>
-
-		<Button class="mt-7 h-10 rounded-full px-6 py-3 font-bold" size="lg"
-			>Start Comparing <span><ChevronDown /></span></Button
-		>
-	</div>
-
-	<div class="flex items-end gap-8">
-		<div class=" max-w-40 space-y-1 rounded-3xl bg-foreground p-4 text-background">
+	<div class="grid grid-cols-[minmax(0,1.2fr)_minmax(0,1fr)] grid-rows-[1fr_auto] gap-4">
+		<div class="col-start-1 row-start-1 self-center py-8">
 			<div class="flex items-center gap-1.5">
 				<div
 					class="size-2 animate-pulse rounded-full bg-accent shadow-[0_0_30px_5px] shadow-accent/70"
 				></div>
-				<span class="font-font-heading text-xs font-medium tracking-wider text-muted uppercase"
-					>StockX leads</span
+				<span class="text-xs font-medium tracking-wider text-muted-foreground uppercase"
+					>THE SMARTER WAY TO BUY SNEAKERS</span
 				>
 			</div>
 
-			<p class="font-heading font-bold">by $8</p>
+			<div class="space-y-3">
+				<h1 class="font-heading text-4xl font-bold tracking-tight md:text-7xl">
+					<span> One pair. </span>
+					<br />
+					<span class="text-muted-foreground">Two markets.</span>
+					<br />
+					<span> One clear choice. </span>
+				</h1>
+				<p>
+					STRIDE compares sneaker prices across StockX and GOAT, so you can quickly see where to
+					buy, what you'll pay, and when the price is right.
+				</p>
+			</div>
+
+			<Button href="/#collections" class="mt-7 h-10 rounded-full px-6 py-3 font-bold" size="lg"
+				>Start Comparing <span><ChevronDown /></span></Button
+			>
 		</div>
 
-		<article
-			class="relative max-w-96 -translate-y-1 rotate-6 rounded-xl border bg-card p-4 shadow-lg/10"
-		>
-			<div class="absolute top-4 left-4">
-				<div class="rounded-full bg-muted px-3 py-1 text-muted-foreground">
-					<p class="font-heading text-xs font-bold">Nike</p>
-				</div>
-			</div>
-			<div class="flex h-full flex-col items-center">
-				<img class="max-h-full max-w-full object-contain" src={nike} alt="Nike Air Zoom GT Cut 4" />
+		<div class="relative col-start-2 row-start-1 hidden md:block">
+			<div class="flex items-end gap-8">
+				<div class=" max-w-40 space-y-1 rounded-3xl bg-foreground p-4 text-background">
+					<div class="flex items-center gap-1.5">
+						<div
+							class="size-2 animate-pulse rounded-full bg-accent shadow-[0_0_30px_5px] shadow-accent/70"
+						></div>
+						<span class="font-font-heading text-xs font-medium tracking-wider text-muted uppercase"
+							>StockX leads</span
+						>
+					</div>
 
-				<!-- Card content -->
-				<div class="flex w-full flex-1 px-3 py-8">
-					<div class="flex-1">
-						<div class="w-3/4">
-							<h3 class="font-heading font-bold tracking-tight">Nike Air Zoom GT Cut 4</h3>
+					<p class="font-heading font-bold">by $8</p>
+				</div>
+
+				<article
+					class="relative max-w-96 -translate-y-1 rotate-6 rounded-xl border bg-card p-4 shadow-lg/10"
+				>
+					<div class="absolute top-4 left-4">
+						<div class="rounded-full bg-muted px-3 py-1 text-muted-foreground">
+							<p class="font-heading text-xs font-bold">Nike</p>
 						</div>
-						<p class="mt-1 text-xs text-muted-foreground">University Red Black</p>
 					</div>
-					<div class="text-end">
-						<p class="font-heading font-bold text-foreground">$128</p>
-						<p class="mt-1 font-heading text-xs tracking-tight text-muted-foreground">best ask</p>
+					<div class="flex h-full flex-col items-center">
+						<img
+							class="max-h-full max-w-full object-contain"
+							src={nike}
+							alt="Nike Air Zoom GT Cut 4"
+						/>
+
+						<!-- Card content -->
+						<div class="flex w-full flex-1 px-3 py-8">
+							<div class="flex-1">
+								<div class="w-3/4">
+									<h3 class="font-heading font-bold tracking-tight">Nike Air Zoom GT Cut 4</h3>
+								</div>
+								<p class="mt-1 text-xs text-muted-foreground">University Red Black</p>
+							</div>
+							<div class="text-end">
+								<p class="font-heading font-bold text-foreground">$128</p>
+								<p class="mt-1 font-heading text-xs tracking-tight text-muted-foreground">
+									best ask
+								</p>
+							</div>
+						</div>
 					</div>
+				</article>
+			</div>
+		</div>
+
+		<!-- Features -->
+		<div class="col-span-full row-start-2 mt-10 w-full border-t py-8">
+			<div class="flex w-full gap-4">
+				<div class="flex items-center gap-1.5">
+					<Check class="size-4" />
+					<p class="text-xs">Compare all-in totals</p>
 				</div>
-			</div>
-		</article>
-	</div>
 
-	<div class="col-span-full border-t py-4">
-		<div class="flex w-full">
-			<div class="flex items-center gap-1.5">
-				<Check class="size-4" />
-				<p class="text-xs">Compare all-in totals</p>
-			</div>
+				<div class="flex items-center gap-1.5">
+					<Check class="size-4" />
+					<p class="text-xs">Check your size</p>
+				</div>
 
-			<div class="flex items-center gap-1.5">
-				<Check class="size-4" />
-				<p class="text-xs">Check your size</p>
-			</div>
-
-			<div class="flex items-center gap-1.5">
-				<Check class="size-4" />
-				<p class="text-xs">Save your favorite</p>
+				<div class="flex items-center gap-1.5">
+					<Check class="size-4" />
+					<p class="text-xs">Save your favorite</p>
+				</div>
 			</div>
 		</div>
 	</div>
 </main>
 
-<section class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+<section class="grid grid-cols-1 gap-4 px-4 py-8 md:grid-cols-2 lg:grid-cols-3" id="collections">
 	<div class="col-span-full space-y-3">
 		<div class="mb-3 flex items-center gap-1.5">
 			<div
