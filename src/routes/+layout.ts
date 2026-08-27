dded2451366567e9e98b02926172b4d797aea93e
ezlosswm <ezlosswm@gmail.com>
2026-08-27 15:36:12 -0600

    vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,10 @@
 import { PUBLIC_SUPABASE_PUBLISHABLE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import type { LayoutLoad } from './$types';
 import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr';
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
